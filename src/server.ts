b68738bb610b1027ea48bca16b56520f2a28bd6f
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2025-05-19 14:11:45 +0400

    starter code

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+
+const swaggerDocument = YAML.load("./swagger/swagger.yaml");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use((err: any, req: Request, res: Response, next: express.NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  console.error(err);
+  res.status(statusCode).json({ message });
+});
+
+export default app;
